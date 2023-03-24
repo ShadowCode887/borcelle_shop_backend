7d7245f25dc1024d86e3641a91472ab7f63b8555
ShadowCode887 <liontiger851@gmail.com>
2023-03-24 23:23:47 +0200

    Initial commit

---
+++ src/main/java/com/example/borcelle_shop/BorcelleShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.borcelle_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorcelleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorcelleShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/borcelle_shop/BorcelleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.borcelle_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorcelleShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
